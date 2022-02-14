b4ff9eddc79866eff5096dd6f91335f70d077a99
wriy9o <thank@grasp.asia>
2022-02-14 18:56:16 +0700

    update

--- app/Views/front/nav-menu.php
+++ app/Views/front/nav-menu.php
@@ -160,8 +160,7 @@
                         <li><a class="dropdown-item" href="<?= site_url('about/president'); ?>"><?= lang('MenuLang.policy'); ?></a></li>
                         <li><a class="dropdown-item" href="<?= site_url('about/history'); ?>"><?= lang('MenuLang.history'); ?></a></li>
                         <li><a class="dropdown-item" href="<?= site_url('about/directors'); ?>"><?= lang('MenuLang.directors'); ?></a></li>
-                        <li><a class="dropdown-item" href="<?= site_url('about/advisory'); ?>"><?= lang('MenuLang.advisory'); ?></a></li>                        
-                        
+                        <!-- <li><a class="dropdown-item" href="<?= site_url('about/advisory'); ?>"><?= lang('MenuLang.advisory'); ?></a></li> -->
                     </ul>
                 </li>
 
