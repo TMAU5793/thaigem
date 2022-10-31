095ac357ed323fefaf2bcc2f3a8b95ba7af6cef8
T.Mua <thankprat@gmail.com>
2022-10-31 18:48:24 +0700

    update

--- app/Language/en/HomeLang.php
+++ app/Language/en/HomeLang.php
@@ -3,10 +3,10 @@
 // override core en language system validation or define your own en language validation message
 return [
     'newsletterText' => '<strong class="c-darkgold d-block">TRADE</strong>
-        <p class="c-darkgold fs-3">ลงทะเบียนเพื่อรับข่าวสาร</p>',
+        <p class="c-darkgold fs-3">Sign up for news</p>',
     
     'signupText' => '<strong class="c-darkgold d-block">MEMBER</strong>
-        <p class="c-darkgold fs-3">สมัครสมาชิกเพื่อสร้างหน้าร้านออนไลน์และรับสิทธิ์ประโยชน์จากทางสมาคม</p>',
+        <p class="c-darkgold fs-3">Apply for membership to create an online storefront and receive benefits from the association.</p>',
 
     'text1' => 'Lorem Ipsum is simply dummy text and typesetting industry. Lorem Ip the dummy text industry. Lorem Ip dummy text and typesetting industry.',
     'category' => '<strong class="ff-bold">TGJTA Members</strong> <span class="ff-reg"> by Categories</span>',
